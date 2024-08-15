d382a0ee42afcee40ec64c51f64563585ad97b2f
igorrc <igor.ramos.c@hotmail.com>
2024-08-14 23:52:58 -0500

    feature : edit empleyee

--- recursoshumanos-servicio/src/main/java/gnius/recursoshumanos/controlador/EmpleadoControlador.java
+++ recursoshumanos-servicio/src/main/java/gnius/recursoshumanos/controlador/EmpleadoControlador.java
@@ -1,11 +1,13 @@
 package gnius.recursoshumanos.controlador;
 
+import gnius.recursoshumanos.excepcion.RecursoNoEncontradoExcepcion;
 import gnius.recursoshumanos.modelo.Empleado;
 import gnius.recursoshumanos.servicio.IEmpleadoServicio;
 import jakarta.el.ELManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,6 +34,14 @@ public class EmpleadoControlador {
     public Empleado agregarEmpleado(@RequestBody Empleado empleado){
         logger.info("Empleado a agregar : " + empleado);
         return empleadoServicio.guardarEmpleado(empleado);
+    }
+
+    @GetMapping("/empleado/{id}")
+    public ResponseEntity<Empleado> obtenerEmpleadoPorId(@PathVariable Integer id){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if(empleado == null)
+            throw new RecursoNoEncontradoExcepcion("No se encontro el id :"  + id);
+        return ResponseEntity.ok(empleado);
     }
 
 }
